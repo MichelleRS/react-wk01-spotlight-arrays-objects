1e47163dd1b3c0268f6d7b2da4ea78796ab00e6d
Michelle Ross <miross629@gmail.com>
2023-01-10 11:49:59 -0800

    Fourth test passing

--- index.js
+++ index.js
@@ -40,7 +40,13 @@ export const printUserInfo = ({
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {}
+export const getSum = (...rest) => {
+  let sum = 0
+  for (let i of rest) {
+    sum += i
+  }
+  return sum
+}
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
